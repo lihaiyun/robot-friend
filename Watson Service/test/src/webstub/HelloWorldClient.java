d90b65f3f6f99d06a798eaf05afce6035c9cd682
Joy <peh-ziyi@hotmail.com>
2016-06-10 11:38:30 +0800

    test template

---
+++ Watson Service/test/src/webstub/HelloWorldClient.java
@@ -0,0 +1,16 @@
+package webstub;
+
+
+
+public class HelloWorldClient{
+	
+	public static void main(String[] args) {
+	   
+		HelloWorldImplService helloService = new HelloWorldImplService();
+		HelloWorld hello = helloService.getHelloWorldImplPort();
+	
+		System.out.println(hello.getHelloWorldAsString("mkyong"));
+		
+    }
+
+}
